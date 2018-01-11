a63fab307d471ce184b1c53b971325ccc6c0b323
Daria Korotkova <ddm_90@mail.ru>
2018-01-11 03:30:20 +0300

    add hm for 4 week without vue.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,7 +3,101 @@ const $ = require('jquery') // если будет нужен
 
 slider() // инициализируем слайдер
 
-$('.hamburger').click(function(){
-    $(this).next('.nav').toggle()
+$('.hamburger').on('click', function(){
+    $('.nav').toggle()
 })
+
+$('.nav__close').on('click', function(){
+    $('.nav').css('display','none')
+})
+
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner')
+    $spinner.fadeOut()
+    $preloader.delay(350).fadeOut('slow')
+})
+
+$('.buttonit__button').click(function(){
+    $('#flip-toggle').toggleClass('hover')
+    $('.buttonit__button').addClass('disp')
+    $('.user').addClass('disp')
+})
+
+$('.arrow-down').click(function(){
+    $('html, body').animate({scrollTop: $('header').height()+ 50 },'slow')
+
+})
+
+$(function() {
+    $('#block-subtitle').html($('.showing').data('parameter'))
+    $('.next').click(function(){
+        var k = $('.showing').data('parameter')
+        $('#block-subtitle').html(k)
+    })
+})
+
+$(function() {
+    $('#slider__but').attr('href',$('.showing').data('link'))
+    $('.next').click(function(){
+        var k = $('.showing').data('link')
+        $('#slider__but').attr('href',k)
+    })
+})
+
+
+$(function() {
+    $('#slider__but').attr('href',$('.showing').data('link'))
+    $('.prev').click(function(){
+        var k = $('.showing').data('link')
+        $('#slider__but').attr('href',k)
+    })
+})
+
+$(function() {
+    $('#block-subtitle').html($('.showing').data('parameter'))
+    $('.prev').click(function(){
+        var k = $('.showing').data('parameter')
+        $('#block-subtitle').html(k)
+    })
+})
+
+var lastId,
+    topMenu = $('#top-menu'),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    menuItems = topMenu.find('a'),
+    scrollItems = menuItems.map(function() {
+        var item = $($(this).attr('href'))
+        if (item.length) {
+            return item
+        }
+    })
+
+menuItems.click(function(e) {
+    var href = $(this).attr('href'),
+        offsetTop = href === '#' ? 0 : $(href).offset().top - topMenuHeight + 1
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300)
+    e.preventDefault()
+})
+
+$(window).scroll(function() {
+    var fromTop = $(this).scrollTop() + topMenuHeight
+    var cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+            return this
+    })
+
+    cur = cur[cur.length - 1]
+    var id = cur && cur.length ? cur[0].id : ''
+
+    if (lastId !== id) {
+        lastId = id
+        menuItems
+    .parent().removeClass('active')
+    .end().filter('[href=\'#' + id + '\']').parent().addClass('active')
+    }
+})
+
 
--- src/scripts/common/slider.js
+++ src/scripts/common/slider.js
@@ -2,23 +2,41 @@ const slides = document.querySelectorAll('#slides .slide')
 const next = document.getElementById('next')
 const previous = document.getElementById('previous')
 const controls = document.querySelectorAll('.controls')
+const NS = document.querySelectorAll('#slider__next .slider__little-n')
+const PR = document.querySelectorAll('#slider__previus .slider__little-p')
 
 let currentSlide = 0
-
+let currN = 0
+let currP = 0
 // осуществляет переход к слайду номер n (начиная с 0)
 function goToSlide(n){
     slides[currentSlide].className = 'slide'
     currentSlide = (n+slides.length)%slides.length // остаток от деления
-    slides[currentSlide].className = 'slide showing'
+    slides[currentSlide].className = 'slide slider__big showing'
+}
+function goToNext(n){
+    NS[currN].className = 'slider__little-n'
+    currN = (n+NS.length)%NS.length
+    NS[currN].className = 'slider__little-n showing'
+}
+function goToPrev(n){
+    PR[currP].className = 'slider__little-p'
+    currP = (n+PR.length)%PR.length
+    PR[currP].className = 'slider__little-p showing'
 }
 
 // навешивает обработчики событий на элементы next и previous
 function setupListners(){
+
+
     next.onclick = function(){
         goToSlide(currentSlide+1)
+        goToNext(currN+2)
+
     }
     previous.onclick = function(){
         goToSlide(currentSlide-1)
+        goToPrev(currP-1)
     }
 }
 
@@ -26,6 +44,7 @@ function setupListners(){
 function showButtons(){
     for (var i=0; i<controls.length; i++){
         controls[i].style.display = 'inline-block'
+
     }
 }
 
@@ -36,5 +55,6 @@ function sliderInit(){
         showButtons()
     }
 }
+
 
 module.exports = sliderInit
